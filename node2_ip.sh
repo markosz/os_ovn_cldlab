8a571b8fbe5fbefbfdb9b99621d6f45ef82ba607
markosz <32844538+markosz@users.noreply.github.com>
2020-03-06 18:13:39 +0100

    Update node2_ip.sh
    eliminate scp prompt

--- node2_ip.sh
+++ node2_ip.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
  ifconfig | grep "inet " |  awk 'NR == 1 {print $2}' > node2_ip
- sudo scp node2_ip node1:/local/repository/
+ sudo scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null node2_ip node1:/local/repository/
+ 
