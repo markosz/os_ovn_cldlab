9f370d0f94326d770835ee5f51dfa80476759606
markosz <32844538+markosz@users.noreply.github.com>
2020-03-06 18:22:06 +0100

    typos fixed

--- conf_and_run.sh
+++ conf_and_run.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-sudo packstak --gen-answer-file=ans.cfg
+sudo packstack --gen-answer-file=ans.cfg
 
 sudo cp ans.cfg test.cfg
 
 sudo sed -i -re 's/(CONFIG_CEILOMETER_INSTALL=)\w+/\1n/gi' test.cfg
 sudo sed -i -re 's/(CONFIG_AODH_INSTALL=)\w+/\1n/gi' test.cfg
 N2=`sudo cat node2_ip`
-sudo sed -i -re "s/(CONFIG_COMPUTE_HOSTS=.+)/\1,$N2/gi" probe.txt
+sudo sed -i -re "s/(CONFIG_COMPUTE_HOSTS=.+)/\1,$N2/gi" test.cfg
 sudo sed -i -re 's/(CONFIG_KEYSTONE_ADMIN_PW=)\w+/\1adminpass/gi' test.cfg
 sudo sed -i -re 's/(CONFIG_KEYSTONE_DEMO_PW=)\w+/\1demopass/gi' test.cfg
 
-#sudo packstak --answer-file=test.cfg
+sudo packstack --answer-file=test.cfg
